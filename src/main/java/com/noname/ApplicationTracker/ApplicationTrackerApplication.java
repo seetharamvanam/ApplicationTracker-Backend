3cd87861fa65e8a0e8dadae43ae4bb6c520a875f
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2025-08-22 13:13:28 -0500

    Add Spring files for Backend configuration

---
+++ src/main/java/com/noname/ApplicationTracker/ApplicationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.noname.ApplicationTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noname/ApplicationTracker/ApplicationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname.ApplicationTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
